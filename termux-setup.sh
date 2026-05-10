800a3bd7cd0878c3bcc65dfacabe0b5899b8cdeb
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-10 23:58:11 +0545

    initial commit: installing essential packages

---
+++ termux-setup.sh
@@ -0,0 +1,25 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+packages=(
+  eza
+  fd
+  fzf
+  git
+  lua-language-server
+  neovim
+  nodejs
+  ripgrep
+  starship
+  stylua
+  tmux
+  zoxide
+)
+
+for pkg in "${packages[@]}"; do
+  if pkg list-installed 2>/dev/null | grep -q "^$pkg/"; then
+    echo "$pkg already installed, skipping..."
+  else
+    echo "Installing $pkg"
+    pkg install -y "$pkg"
+  fi
+done
