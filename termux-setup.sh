c0b13d02a2f7d4ab6fc02bb4dcf024880f5dd361
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-11 00:20:56 +0545

    cloning required repos

--- termux-setup.sh
+++ termux-setup.sh
@@ -25,7 +25,40 @@ for pkg in "${packages[@]}"; do
   fi
 done
 
+clone() {
+  local item_name="$1"
+  local base_path="$2"
 
+  # Full URL
+  if [[ "$item_name" =~ ^https?:// ]]; then
+    repo_url="$item_name"
+    item_dir="${base_path}/$(basename "$repo_url" .git)"
+  # Username/reponame format
+  elif [[ "$item_name" =~ / ]]; then
+    repo_url="https://github.com/${item_name}.git"
+    item_dir="${base_path}/$(basename "$repo_url" .git)"
+  # Defaults to zsh-users owner
+  else
+    repo_url="https://github.com/bibekbhusal0/${item_name}.git"
+    item_dir="${base_path}/$(basename "$repo_url" .git)"
+  fi
 
-# cloning essential reposoteries.
-git clone https://github.com/basecamp/omarchy ~/omarchy --depth=10
+  # Check if the item directory already exists
+  if [ -d "$item_dir" ]; then
+    echo "$item_name directory $item_dir already exists. Skipping..."
+  else
+    git clone "$repo_url" "$item_dir"
+  fi
+}
+
+mkdir -p ~/Code 
+clone omarchy-overrides ~/Code/omarchy-overrides
+clone basecamp/omarchy ~/Code/omarchy
+mkdir ~/.config
+clone neovim-kickstart-config-config ~/.config/nvim
+mkdir -p ~/Code/nvim-plugins
+clone bufstack.nvim ~/Code/nvim-plugins/bufstack.nvim
+clone nvim-shadcn ~/Code/nvim-plugins/nvim-shadcn
+clone nvim-git-utils ~/Code/nvim-plugins/nvim-git-utils
+
+ln -s ~/Code/nvim-plugins/
